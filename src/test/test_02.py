5224c57e7c8ca15df9932b3d69d9ff9b8fbfdcc1
BenSt099 <ben.steinhauer.de@gmail.com>
2023-02-23 16:13:18 +0100

    added tests

--- src/test/test_02.py
+++ src/test/test_02.py
@@ -1,5 +1,4 @@
 import sys
-import pytest
 import numpy as np
  
 sys.path.append('../src/')
@@ -22,3 +21,22 @@ def test_0203():
     pmmobj.put("key1",[1,2,3])
     pmmobj.put("key2",[4,5,6])
     assert len(pmmobj.values()) == 2
+
+def test_0204():
+    pmmobj = PyMultiMap()
+    pmmobj.put("key1",[1,2,3])
+    pmmobj.put("key2",[4,5,6])
+    pmmobj.remove("key2")
+    assert pmmobj.size() == 1
+
+def test_0205():
+    pmmobj = PyMultiMap()
+    pmmobj.put("key1",[1,2,3])
+    pmmobj.put("key2",[4,5,6])
+    pmmobj.update("key2",np.array([5,6,7]))
+    assert list(pmmobj.get("key2")) == list(np.array([5,6,7]))
+
+def test_0206():
+    pmmobj = PyMultiMap()
+    pmmobj.put("key1",[1,2,3])
+    assert pmmobj.get("key2") == None
